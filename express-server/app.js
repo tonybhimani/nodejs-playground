96b79616015aa3c9a23bbd5536b6120954ff78b2
Tony Bhimani <tonybhimani@users.noreply.github.com>
2023-09-02 18:56:42 -0700

    example: minimal express server

---
+++ express-server/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+// obsolete with express.static in use
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.listen(port, () => {
+  console.log(`Express server listening on port ${port}`)
+});
